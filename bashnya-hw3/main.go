45e37098e4d1018cb8eb9052e50fa949a89912be
SBmstu <bsea24u803@student.bmstu.ru>
2025-10-27 22:51:52 +0300

    errors added

--- bashnya-hw3/main.go
+++ bashnya-hw3/main.go
@@ -3,32 +3,52 @@ package main
 import (
 	"bashnya-hw3/stack"
 	"bashnya-hw3/util"
+	"fmt"
+	"os"
 )
 
 func main() {
+	var err error = run();
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "\nОшибка: %v\n", err); // разобраться со спецификаторами
+		os.Exit(1);
+	}
+
+	os.Exit(0); // Как лучше обрабатывать ошибки?
+}
+
+func run() error {
 	var stack stack.Stack_int; // не будет ругаться на имя переменной?
 	stack.Init();
 
+	var err error;
 	var choice int = -1;
 	for (choice != 0) {
-		err := util.ChooseOption(&choice);
+		err = util.ChooseOption(&choice);
 		if (err != nil) {
-			return ;
+			return err;
 		}
 
 		switch choice {
 		case util.EXIT:
-			return
+			return nil;
+
 		case util.PUSH:
-			return
+
+			return err;
+
 		case util.POP:
-			return
+			return err;
 		case util.SIZE:
-			return
+			return err;
+
 		case util.CLEAR:
-			return
+			return err;
+
 		case util.ADD:
-			return
+			return err
 		}
 	}
+
+	return nil;
 }
